236d2d3cffd1edef0fcc931d413002ee02422a45
Gantulga G <limited.tulgaa@gmail.com>
2023-09-07 21:09:26 +0800

    setup fix

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py2048",
+    name="Py2048",
     version="1.0.4",
     author="Gantulga G",
-    author_email="limited.tulgaa@gmail.com>",
+    author_email="limited.tulgaa@gmail.com",
     description="Python implementation of 2048 game",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Gantulga9480/Game",
-    packages=['py2048'],
+    url="https://github.com/Gantulga9480/Py2048",
+    packages=['Py2048'],
     license='MIT',
     install_requires=['pygame', 'numpy'],
 )
